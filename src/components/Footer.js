1bd90749c8ea13f050094ed69c2e7d4f641e720f
Judith Dea <JudithFoe@gmail.com>
2018-09-19 20:04:32 -0400

    minor layout adjustment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="page-footer fixed-bottom font-small bg-secondary">
-      <div class="footer-copyright text-center">
-        © 2018 Copyright:
-        <p>Judith Dea</p>
-      </div>
+    <footer class="page-footer fixed-bottom font-small bg-secondary py-2">
+      <p class="footer-copyright text-center m-0">© 2018 -- Judith Dea</p>
     </footer>
   );
 };
