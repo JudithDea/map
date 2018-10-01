5bf5705d575720364eeeff5fcbc8963b49f9ea39
Judith Dea <JudithFoe@gmail.com>
2018-09-30 21:16:15 -0400

    added sidebar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,104 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ShowMap from "./components/ShowMap";
+import SideNav, {
+  Toggle,
+  Nav,
+  NavItem,
+  NavIcon,
+  NavText
+} from "@trendmicro/react-sidenav";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <SideNav
+          onSelect={selected => {
+            // Add your code here
+          }}
+        >
+          <SideNav.Toggle />
+          <SideNav.Nav defaultSelected="home">
+            <NavItem eventKey="home">
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-home"
+                  style={{ fontSize: "1.75em" }}
+                />
+              </NavIcon>
+              <NavText>
+                <form className="form-inline my-2 my-lg-0">
+                  <input
+                    className="form-control mr-sm-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                </form>
+              </NavText>
+            </NavItem>
+            <NavItem>
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-line-chart"
+                  style={{ fontSize: "1.75em" }}
+                />
+              </NavIcon>
+              <NavText>
+                <a
+                  href="https://en.wikipedia.org/wiki/Stolperstein"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="nav-link"
+                >
+                  What are Stolpersteine?
+                </a>
+              </NavText>
+            </NavItem>
+            <NavItem>
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-line-chart"
+                  style={{ fontSize: "1.75em" }}
+                />
+              </NavIcon>
+              <NavText>
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="nav-link"
+                >
+                  Translating Stolpersteine
+                </a>
+              </NavText>
+            </NavItem>
+            <NavItem>
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-line-chart"
+                  style={{ fontSize: "1.75em" }}
+                />
+              </NavIcon>
+              <NavText>
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="nav-link"
+                >
+                  About this page
+                </a>
+              </NavText>
+            </NavItem>
+          </SideNav.Nav>
+        </SideNav>
         <ShowMap />
         <Footer />
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
           <h1>Neighborhood Map</h1>
           <p>"Stolpersteine" in Bonn, Germany</p>
         </a>
-        <ul className="navbar-nav mr-2">
+        {/* <ul className="navbar-nav mr-2">
           <li className="nav-item">
             <a
               href="https://en.wikipedia.org/wiki/Stolperstein"
@@ -29,16 +29,16 @@ const Header = () => {
               About this page
             </a>
           </li>
-        </ul>
+        </ul> */}
       </div>
-      <form className="form-inline my-2 my-lg-0">
+      {/* <form className="form-inline my-2 my-lg-0">
         <input
           className="form-control mr-sm-2"
           type="search"
           placeholder="Search"
           aria-label="Search"
         />
-      </form>
+      </form> */}
     </nav>
   );
 };
