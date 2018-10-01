5c15cd1db6b561d36203e19fbf8368a75cf236aa
Judith Dea <JudithFoe@gmail.com>
2018-10-01 15:07:37 -0400

    moved state over to App.js so it can be passed into more  components from here

--- src/App.js
+++ src/App.js
@@ -15,35 +15,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
 class App extends Component {
+  state = {
+    steine: []
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(
+      "http://overpass-api.de/api/interpreter?data=[out:json]%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%20and%20type%3Anode%20in%20%22Bonn%22%20%E2%80%9D%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20fetch%20area%20%E2%80%9CBonn%E2%80%9D%20to%20search%20in%0Aarea%283600062508%29-%3E.searchArea%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%E2%80%9D%0A%20%20node%5B%22memorial%3Atype%22%3D%22stolperstein%22%5D%28area.searchArea%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+    )
+      .then(res => res.json())
+      .then(steine => {
+        this.setState({ steine: steine.elements });
+      })
+      .catch(err => console.log("There was a problem loading data", err));
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <SideNav
-          onSelect={selected => {
-            // Add your code here
-          }}
-        >
+        <SideNav onSelect={selected => {}}>
           <SideNav.Toggle />
           <SideNav.Nav defaultSelected="home">
-            <NavItem eventKey="home">
-              <NavIcon>
-                <i
-                  className="fa fa-fw fa-home"
-                  style={{ fontSize: "1.75em" }}
-                />
-              </NavIcon>
-              <NavText>
-                <form className="form-inline my-2 my-lg-0">
-                  <input
-                    className="form-control mr-sm-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                  />
-                </form>
-              </NavText>
-            </NavItem>
             <NavItem>
               <NavIcon>
                 <i
@@ -98,9 +95,27 @@ class App extends Component {
                 </a>
               </NavText>
             </NavItem>
+            <NavItem eventKey="home">
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-home"
+                  style={{ fontSize: "1.75em" }}
+                />
+              </NavIcon>
+              <NavText>
+                <form className="form-inline my-2 my-lg-0">
+                  <input
+                    className="form-control mr-sm-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                </form>
+              </NavText>
+            </NavItem>
           </SideNav.Nav>
         </SideNav>
-        <ShowMap />
+        <ShowMap steine={this.state.steine} />
         <Footer />
       </div>
     );
--- src/components/ShowMap1.js
+++ src/components/ShowMap1.js
@@ -3,97 +3,48 @@ import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 class ShowMap extends Component {
   state = {
-    steine: [
-      {
-        id: 732473819,
-        position: [50.73863, 7.1174091],
-        name: "Erna Kaufmann",
-        date_of_birth: "1903",
-        text:
-          "Hier wohnte Erna Kaufmann geb. Oster Jg. 1903 deportiert 1942 Ziel unbekannt verschollen",
-        image:
-          "File:Stolpersteins_Karl_Kaufmann%2C_Ludwig_Kaufmann%2C_Ruth_Kaufmann%2C_Erna_Kaufmann_Crossing_Friedrich-Breuer-Stra%C3%9Fe%2C_Gottfried-Claren-Stra%C3%9Fe_Bonn.JPG"
-      },
-      {
-        id: 732500741,
-        position: [50.7385646, 7.1167357],
-        name: "Ella Herz",
-        date_of_birth: "1867",
-        text:
-          "Hier wohnte Ella Herz geb. Neumann Jg. 1867 deportiert 1942 Theresienstadt verschollen in Treblinka",
-        image:
-          "File:Stolpersteins_Alfred_Herz%2C_Lucie_Herz%2C_Ella_Herz%2C_Ingeborg_Herz_Friedrich-Breuer-Stra%C3%9Fe_25_Bonn.JPG"
-      },
-      {
-        id: 811772382,
-        position: [50.7270855, 7.0972615],
-        name: "Cäsar Bukofzer",
-        date_of_birth: "1886-09-08",
-        text:
-          "Hier wohnte Cäsar Bukofzer Jg. 1886 deportiert 1942 verschollen in Auschwitz",
-        image:
-          "File:Stolpersteine_C%C3%A4sar_Bukofzer%2C_Julie_Bukofzer%2C_Argelanderstra%C3%9Fe_10%2C_Bonn.JPG"
-      },
-      {
-        id: 814018389,
-        position: [50.6911847, 7.1599095],
-        name: "Sara Auerbach",
-        date_of_birth: "1871",
-        text:
-          "Hier wohnte Sara Auerbach Jg. 1871 deportiert 1942 Theresienstadt tot 16.1.1943",
-        image: "File:Stolperstein_Sara_Auerbach_Viktoriastra%C3%9Fe_3_Bonn.JPG"
-      },
-      {
-        id: 830374061,
-        position: [50.7371136, 7.0962884],
-        name: "Paula Jonas",
-        date_of_birth: "1892",
-        text: "Hier wohnte Paula Jonas Jg. 1892 deportiert 1941 tot",
-        image:
-          "File:Stolperstein_Paula_Jonas%2C_Breite_Stra%C3%9Fe_8%2C_Bonn.JPG"
-      },
-      {
-        id: 831251686,
-        position: [50.7105994, 7.1104217],
-        name: "Bernhard Marx",
-        date_of_birth: "1897",
-        text:
-          "Hier wohnte Bernhard Marx Jg. 1897 deportiert 20.7.1942 Minsk ermordet 24.7.1942",
-        image:
-          "File:Stolpersteins_Bernhard_Marx%2C_Erna_Marx%2C_Julie_Marx%2C_Helena_Marx_Burbacher_Stra%C3%9Fe_251_Bonn.JPG"
-      },
-      {
-        id: 831274352,
-        position: [50.7247345, 7.0968286],
-        name: "Siegmund Fauerbach",
-        date_of_birth: "1858",
-        text:
-          "Hier wohnte Siegmund Fauerbach Jg. 1858 interniert 1942 Sammellager Bonn-Endenich tot 23.3.1942",
-        image:
-          "File:Stolpersteine_Siegmund_Fauerbach%2C_Eisabeth_Fauerbach%2C_Helene_Fauerbach%2C_Dorothea_Fauerbach%2C_Hans_Peter_Fauerbach%2C_Kurf%C3%BCrstenstra%C3%9Fe_33%2C_Bonn.JPG"
-      },
-      {
-        id: 940477131,
-        position: [50.682074, 7.1486897],
-        name: "Bertha Frenkel",
-        date_of_birth: "1873",
-        text:
-          "Hier wohnte Bertha Frenkel geb. Drücker Jg. 1873 interniert 1942 Kloster Endenich tot 3.2.1942",
-        image:
-          "File:Stolpersteins_Raphael_Frenkel%2C_Bertha_Frenkel%2C_Erna_Frenkel_Crossing_Burgstra%C3%9Fe_Im_%C3%84uelchen_Bonn.JPG"
-      }
-    ]
+    steine: []
   };
 
+  // async componentDidMount() {
+  //   try {
+  //     const response = await fetch(
+  //       "http://overpass-api.de/api/interpreter?data=[out:json]%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%20and%20type%3Anode%20in%20%22Bonn%22%20%E2%80%9D%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20fetch%20area%20%E2%80%9CBonn%E2%80%9D%20to%20search%20in%0Aarea%283600062508%29-%3E.searchArea%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%E2%80%9D%0A%20%20node%5B%22memorial%3Atype%22%3D%22stolperstein%22%5D%28area.searchArea%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+  //     );
+  //     const json = await response.json();
+  //     console.log({ data: json });
+  //   } catch (error) {
+  //     console.log("Oops. Something went wrong.", error);
+  //   }
+  // }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(
+      "http://overpass-api.de/api/interpreter?data=[out:json]%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%20and%20type%3Anode%20in%20%22Bonn%22%20%E2%80%9D%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20fetch%20area%20%E2%80%9CBonn%E2%80%9D%20to%20search%20in%0Aarea%283600062508%29-%3E.searchArea%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%E2%80%9D%0A%20%20node%5B%22memorial%3Atype%22%3D%22stolperstein%22%5D%28area.searchArea%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+    )
+      .then(res => res.json())
+      .then(steine => {
+        this.setState({ steine: steine.elements });
+      })
+      .catch(err => console.log("There was a problem loading data", err));
+  }
+
   render() {
-    // const position = [this.state.lat, this.state.lon];
+    let position = [""];
+    // console.log(this.state.steine);
     const mapPosition = [50.729203, 7.099475];
+
     return (
       <div id="mapid" role="application">
         <Map
           center={mapPosition}
           zoom={14}
           style={{
+            //Styling for window-sized map
             top: "0",
             bottom: "0",
             left: "0",
@@ -105,30 +56,34 @@ class ShowMap extends Component {
             attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-
-          {this.state.steine.map(stein => (
-            <Marker position={stein.position} key={stein.id}>
-              <Popup>
-                <p className="mb-0" style={{ fontWeight: "bold" }}>
-                  {stein.name}
-                </p>
-                <p className="mb-o">
-                  <span style={{ fontWeight: "bold" }}>Date of birth: </span>
-                  {stein.date_of_birth}
-                </p>
-                <p className="mb-o">
-                  <span style={{ fontWeight: "bold" }}>Text on marker: </span>
-                  {stein.text}
-                </p>
-                <img
-                  src={`https://de.wikipedia.org/wiki/Liste_der_Stolpersteine_in_Bonn#/media/${
-                    stein.image
-                  }`}
-                  alt="Image"
-                />
-              </Popup>
-            </Marker>
-          ))}
+          {this.state.steine.map(stein => {
+            position = [stein.lat, stein.lon];
+            return (
+              <Marker position={position} key={stein.id}>
+                <Popup>
+                  <p className="mb-0">
+                    <span style={{ fontWeight: "bold" }}>
+                      {stein.tags.name}
+                    </span>
+                    <br />
+                    Date of birth: {stein.tags["person:date_of_birth"]}
+                    <br />
+                    Date of death: {stein.tags["person:date_of_death"]}
+                  </p>
+                  <p className="mb-0">"{stein.tags["memorial:text"]}" </p>
+                  <a
+                    href={`https://de.wikipedia.org/wiki/Liste_der_Stolpersteine_in_Bonn#/media/${
+                      stein.tags.image
+                    }`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Click here to see an image of the marker
+                  </a>
+                </Popup>
+              </Marker>
+            );
+          })}
         </Map>
       </div>
     );
