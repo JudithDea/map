b2847ee0cd3eda246471924e875e2edb31c38bf1
Judith Dea <JudithFoe@gmail.com>
2018-10-01 19:52:57 -0400

    added hamburger, adjusted font colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="page-footer fixed-bottom font-small bg-secondary py-2">
+    <footer className="page-footer fixed-bottom font-small bg-secondary py-2 text-light">
       <p className="footer-copyright text-center m-0">
         © 2018 -- Judith Dea -- Map and Stolperstein data from
         www.openstreetmap.org. Thank you!
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,42 +3,16 @@ import React from "react";
 const Header = () => {
   return (
     <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-secondary py-0">
+      <button className="bg-secondary border-0">
+        <i className="fas fa-bars fa-2x text-light" />
+      </button>
+      <div />
       <div className="container">
         <a href="/" className="navbar-brand py-2">
           <h1>Neighborhood Map</h1>
-          <p>"Stolpersteine" in Bonn, Germany</p>
+          <p className="mb-0">"Stolpersteine" in Bonn, Germany</p>
         </a>
-        {/* <ul className="navbar-nav mr-2">
-          <li className="nav-item">
-            <a
-              href="https://en.wikipedia.org/wiki/Stolperstein"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="nav-link"
-            >
-              What are Stolpersteine?
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/" className="nav-link">
-              Translation cheat sheet
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/" className="nav-link">
-              About this page
-            </a>
-          </li>
-        </ul> */}
       </div>
-      {/* <form className="form-inline my-2 my-lg-0">
-        <input
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-      </form> */}
     </nav>
   );
 };
