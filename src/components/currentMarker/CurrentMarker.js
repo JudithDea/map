3a60031fe259d76f8ebecf59b48cefde1e709925
Judith Dea <JudithFoe@gmail.com>
2018-10-08 03:34:58 -0400

    added test version to create open by default Popup for active marker

--- src/components/currentMarker/CurrentMarker.js
+++ src/components/currentMarker/CurrentMarker.js
@@ -4,48 +4,56 @@ import { renderToStaticMarkup } from "react-dom/server";
 import { divIcon } from "leaflet";
 import IconRed from "./IconRed";
 
-const CurrentMarker = props => {
-  const iconMarkup = renderToStaticMarkup(<IconRed />);
-  const activeMarkerIcon = divIcon({
-    html: iconMarkup
-  });
+class CurrentMarker extends Marker {
+  componentDidMount() {
+    super.componentDidMount();
+    this.leafletElement.openPopup();
+  }
 
-  let imageText;
-  let position = [""];
+  render() {
+    const iconMarkup = renderToStaticMarkup(<IconRed />);
+    const activeMarkerIcon = divIcon({
+      html: iconMarkup
+    });
 
-  return props.activeStein.map(stein => {
-    position = [stein.lat, stein.lon];
+    let imageText;
+    let position = [""];
 
-    return (
-      <Marker
-        position={position}
-        key={stein.id}
-        icon={activeMarkerIcon}
-        zIndexOffset={200}
-      >
-        <Popup>
-          <p className="mb-0">
-            <span style={{ fontWeight: "bold" }}>{stein.tags.name}</span>
-            <br />
-            Date of birth: {stein.tags["person:date_of_birth"]}
-            <br />
-            Date of death: {stein.tags["person:date_of_death"]}
-          </p>
-          <p className="mb-0">"{stein.tags["memorial:text"]}" </p>
-          <p>
-            <a
-              href={`https://de.wikipedia.org/wiki/Liste_der_Stolpersteine_in_Bonn#/media/${
-                stein.tags.image
-              }`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fas fa-external-link-alt" /> {imageText}
-            </a>
-          </p>
-        </Popup>
-      </Marker>
-    );
-  });
-};
+    return this.props.activeStein.map(stein => {
+      position = [stein.lat, stein.lon];
+
+      return (
+        <Marker
+          position={position}
+          key={stein.id}
+          icon={activeMarkerIcon}
+          zIndexOffset={200}
+        >
+          <Popup>
+            <p className="mb-0">
+              <span style={{ fontWeight: "bold" }}>{stein.tags.name}</span>
+              <br />
+              Date of birth: {stein.tags["person:date_of_birth"]}
+              <br />
+              Date of death: {stein.tags["person:date_of_death"]}
+            </p>
+            <p className="mb-0">"{stein.tags["memorial:text"]}" </p>
+            <p>
+              <a
+                href={`https://de.wikipedia.org/wiki/Liste_der_Stolpersteine_in_Bonn#/media/${
+                  stein.tags.image
+                }`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-external-link-alt" /> {imageText}
+              </a>
+            </p>
+          </Popup>
+        </Marker>
+      );
+    });
+  }
+}
+
 export default CurrentMarker;
---
+++ src/components/currentMarker/CurrentMarker1.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Marker, Popup } from "react-leaflet";
+import { renderToStaticMarkup } from "react-dom/server";
+import { divIcon } from "leaflet";
+import IconRed from "./IconRed";
+
+const CurrentMarker = props => {
+  const iconMarkup = renderToStaticMarkup(<IconRed />);
+  const activeMarkerIcon = divIcon({
+    html: iconMarkup
+  });
+
+  let imageText;
+  let position = [""];
+
+  return props.activeStein.map(stein => {
+    position = [stein.lat, stein.lon];
+
+    return (
+      <Marker
+        position={position}
+        key={stein.id}
+        icon={activeMarkerIcon}
+        zIndexOffset={200}
+      >
+        <Popup>
+          <p className="mb-0">
+            <span style={{ fontWeight: "bold" }}>{stein.tags.name}</span>
+            <br />
+            Date of birth: {stein.tags["person:date_of_birth"]}
+            <br />
+            Date of death: {stein.tags["person:date_of_death"]}
+          </p>
+          <p className="mb-0">"{stein.tags["memorial:text"]}" </p>
+          <p>
+            <a
+              href={`https://de.wikipedia.org/wiki/Liste_der_Stolpersteine_in_Bonn#/media/${
+                stein.tags.image
+              }`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fas fa-external-link-alt" /> {imageText}
+            </a>
+          </p>
+        </Popup>
+      </Marker>
+    );
+  });
+};
+export default CurrentMarker;
