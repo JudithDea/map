ba4d16992b05bd53a84e9cf619bf9746e19f0c7f
Judith Dea <JudithFoe@gmail.com>
2018-10-10 01:43:12 -0400

    adjusted header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,11 +12,9 @@ const Header = props => {
         <i className="fas fa-bars fa-2x text-light" />
       </button>
       <div />
-      <div className="container">
-        <a href="/" className="navbar-brand py-2">
-          <h1 className="here-lived">Here lived...</h1>
-          <p className="mb-0">"Stolpersteine" in Bonn, Germany</p>
-        </a>
+      <div className="w-100 text-center text-white">
+        <h1 className="here-lived pt-4">Here lived ...</h1>
+        <h3 className=" pb-1">"Stolpersteine" in Bonn, Germany</h3>
       </div>
     </nav>
   );
