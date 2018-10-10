deafa3654493f3da55b532e0ad070efecffc7439
Judith Dea <JudithFoe@gmail.com>
2018-10-10 00:51:21 -0400

    moved loop method to render markers over to showmap to dedicate DefaultMarker to only work Leaflet method for open popup on selected marker

--- src/components/ShowMap.js
+++ src/components/ShowMap.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { renderToStaticMarkup } from "react-dom/server";
+import { divIcon } from "leaflet";
+import IconBlue from "./IconBlue";
+import IconRed from "./IconRed";
 
 import DefaultMarker from "./defaultMarker/DefaultMarker";
 
 const ShowMap = props => {
+  const defaultIconMarkup = renderToStaticMarkup(<IconBlue />);
+  const defaultMarkerIcon = divIcon({ html: defaultIconMarkup });
+  const activeIconMarkup = renderToStaticMarkup(<IconRed />);
+  const activeMarkerIcon = divIcon({ html: activeIconMarkup });
   // let position = [""];
   // console.log(this.state.steine);
   const mapPosition = [50.729203, 7.099475];
@@ -27,11 +35,55 @@ const ShowMap = props => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        <DefaultMarker
+        {props.steine.map(stein => {
+          const position = [stein.lat, stein.lon];
+          let imageText;
+          if (stein.tags.image) {
+            imageText = "Click here for a photo of the marker ";
+          }
+          const isActive = stein.id === props.activeStein;
+
+          return (
+            <Marker
+              position={position}
+              key={stein.id}
+              id={stein.id}
+              icon={isActive ? activeMarkerIcon : defaultMarkerIcon}
+              active={isActive}
+              currentMarkerClickHandler={props.currentMarkerClickHandler}
+              zIndexOffset={isActive ? 200 : 100}
+              click={() => props.currentMarkerClickHandler(stein.id)}
+            >
+              <Popup>
+                <p className="mb-0">
+                  <span style={{ fontWeight: "bold" }}>{stein.tags.name}</span>
+                  <br />
+                  Date of birth: {stein.tags["person:date_of_birth"]}
+                  <br />
+                  Date of death: {stein.tags["person:date_of_death"]}
+                </p>
+                <p className="mb-0">"{stein.tags["memorial:text"]}" </p>
+                <p>
+                  <a
+                    href={`https://de.wikipedia.org/wiki/Liste_der_Stolpersteine_in_Bonn#/media/${
+                      stein.tags.image
+                    }`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fas fa-external-link-alt" /> {imageText}
+                  </a>
+                </p>
+              </Popup>
+            </Marker>
+          );
+        })}
+
+        {/* <DefaultMarker
           steine={props.steine}
           defaultSteine={props.defaultSteine}
           activeStein={props.activeStein}
-        />
+        /> */}
         {/* <CurrentMarker activeStein={props.activeStein} steine={props.steine} /> */}
       </Map>
     </div>
