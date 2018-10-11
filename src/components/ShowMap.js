e84f67fd65d58cb0c8613ff2f06a55cc0b0b521f
Judith Dea <JudithFoe@gmail.com>
2018-10-10 21:36:55 -0400

    turned ShowMap into class component to be able to implement state for centering map on sidebar click

--- src/components/ShowMap.js
+++ src/components/ShowMap.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Map, TileLayer, Popup } from "react-leaflet";
 import { renderToStaticMarkup } from "react-dom/server";
 import { divIcon } from "leaflet";
@@ -7,81 +7,85 @@ import IconRed from "./IconRed";
 
 import DefaultMarker from "./defaultMarker/DefaultMarker";
 
-const ShowMap = props => {
-  const defaultIconMarkup = renderToStaticMarkup(<IconBlue />);
-  const defaultMarkerIcon = divIcon({ html: defaultIconMarkup });
-  const activeIconMarkup = renderToStaticMarkup(<IconRed />);
-  const activeMarkerIcon = divIcon({ html: activeIconMarkup });
-  // let position = [""];
-  // console.log(this.state.steine);
-  const mapPosition = [50.716445, 7.131602];
+class ShowMap extends Component {
+  render() {
+    const defaultIconMarkup = renderToStaticMarkup(<IconBlue />);
+    const defaultMarkerIcon = divIcon({ html: defaultIconMarkup });
+    const activeIconMarkup = renderToStaticMarkup(<IconRed />);
+    const activeMarkerIcon = divIcon({ html: activeIconMarkup });
+    // let position = [""];
+    // console.log(this.state.steine);
+    const mapPosition = [50.716884, 7.104834];
 
-  return (
-    <div id="mapid" role="application">
-      <Map
-        center={mapPosition}
-        zoom={12.5}
-        zIndexOffset={100}
-        style={{
-          //Styling for window-sized map
-          top: "0",
-          bottom: "0",
-          left: "0",
-          right: "0",
-          position: "fixed"
-        }}
-      >
-        <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+    return (
+      <div id="mapid" role="application">
+        <Map
+          center={mapPosition}
+          zoom={12.5}
+          zIndexOffset={100}
+          style={{
+            //Styling for window-sized map
+            top: "0",
+            bottom: "0",
+            left: "0",
+            right: "0",
+            position: "fixed"
+          }}
+        >
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
 
-        {props.steine.map(stein => {
-          const position = [stein.lat, stein.lon];
-          let imageText;
-          if (stein.tags.image) {
-            imageText = "Click here for a photo of the marker ";
-          }
-          const isActive = stein.id === props.activeStein;
+          {this.props.steine.map(stein => {
+            const position = [stein.lat, stein.lon];
+            let imageText;
+            if (stein.tags.image) {
+              imageText = "Click here for a photo of the marker ";
+            }
+            const isActive = stein.id === this.props.activeStein;
 
-          return (
-            <DefaultMarker
-              position={position}
-              key={stein.id}
-              id={stein.id}
-              icon={isActive ? activeMarkerIcon : defaultMarkerIcon}
-              active={isActive}
-              currentMarkerClickHandler={props.currentMarkerClickHandler}
-              zIndexOffset={isActive ? 200 : 100}
-              click={() => props.currentMarkerClickHandler(stein.id)}
-            >
-              <Popup>
-                <p className="mb-0">
-                  <span style={{ fontWeight: "bold" }}>{stein.tags.name}</span>
-                  <br />
-                  Date of birth: {stein.tags["person:date_of_birth"]}
-                  <br />
-                  Date of death: {stein.tags["person:date_of_death"]}
-                </p>
-                <p className="mb-0">"{stein.tags["memorial:text"]}" </p>
-                <p>
-                  <a
-                    href={`https://de.wikipedia.org/wiki/Liste_der_Stolpersteine_in_Bonn#/media/${
-                      stein.tags.image
-                    }`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fas fa-external-link-alt" /> {imageText}
-                  </a>
-                </p>
-              </Popup>
-            </DefaultMarker>
-          );
-        })}
-      </Map>
-    </div>
-  );
-};
+            return (
+              <DefaultMarker
+                position={position}
+                key={stein.id}
+                id={stein.id}
+                icon={isActive ? activeMarkerIcon : defaultMarkerIcon}
+                active={isActive}
+                currentMarkerClickHandler={this.props.currentMarkerClickHandler}
+                zIndexOffset={isActive ? 200 : 100}
+                click={() => this.props.currentMarkerClickHandler(stein.id)}
+              >
+                <Popup>
+                  <p className="mb-0">
+                    <span style={{ fontWeight: "bold" }}>
+                      {stein.tags.name}
+                    </span>
+                    <br />
+                    Date of birth: {stein.tags["person:date_of_birth"]}
+                    <br />
+                    Date of death: {stein.tags["person:date_of_death"]}
+                  </p>
+                  <p className="mb-0">"{stein.tags["memorial:text"]}" </p>
+                  <p>
+                    <a
+                      href={`https://de.wikipedia.org/wiki/Liste_der_Stolpersteine_in_Bonn#/media/${
+                        stein.tags.image
+                      }`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i className="fas fa-external-link-alt" /> {imageText}
+                    </a>
+                  </p>
+                </Popup>
+              </DefaultMarker>
+            );
+          })}
+        </Map>
+      </div>
+    );
+  }
+}
 
 export default ShowMap;
