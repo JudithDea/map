50f8a0e22b129bd3b3f4ddfe9dab84e49226eb14
Judith Dea <JudithFoe@gmail.com>
2018-10-12 12:24:08 -0400

    trying to prevent mixed content

--- src/App.js
+++ src/App.js
@@ -25,13 +25,13 @@ class App extends Component {
 
   fetchData() {
     fetch(
-      "http://overpass-api.de/api/interpreter?data=[out:json]%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%20and%20type%3Anode%20in%20%22Bonn%22%20%E2%80%9D%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20fetch%20area%20%E2%80%9CBonn%E2%80%9D%20to%20search%20in%0Aarea%283600062508%29-%3E.searchArea%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%E2%80%9D%0A%20%20node%5B%22memorial%3Atype%22%3D%22stolperstein%22%5D%28area.searchArea%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+      "https://overpass-api.de/api/interpreter?data=[out:json]%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%20and%20type%3Anode%20in%20%22Bonn%22%20%E2%80%9D%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20fetch%20area%20%E2%80%9CBonn%E2%80%9D%20to%20search%20in%0Aarea%283600062508%29-%3E.searchArea%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9C%22memorial%3Atype%22%3Dstolperstein%E2%80%9D%0A%20%20node%5B%22memorial%3Atype%22%3D%22stolperstein%22%5D%28area.searchArea%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
     )
       .then(res => res.json())
       .then(steine => {
         this.setState({ steine: steine.elements });
       })
-      .catch(err => alert("There was a problem loading data:", err));
+      .catch(err => alert("There was a problem loading data.", err));
   }
 
   hamburgerClickHandler = () => {
